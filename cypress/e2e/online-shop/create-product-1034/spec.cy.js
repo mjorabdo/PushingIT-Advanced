2a763b9c0589e9c7b9f1f5dac81b615286e69e02
Fabrizio Otranto <Fabryotranto@gmail.com>
2024-02-08 16:36:27 -0300

    Add folder and tests

--- cypress/e2e/online-shop/create-product-1034/spec.cy.js
+++ cypress/e2e/online-shop/create-product-1034/spec.cy.js
@@ -1,4 +1,4 @@
-describe('Online Shop ', () => {
+describe('Crear producto - Online Shop ', () => {
 
     it('Deberia permitir al usuario crear un producto', () => {
         cy.log('Crear un producto')
--- cypress/e2e/waits/first-message-2344/spec.cy.js
+++ cypress/e2e/waits/first-message-2344/spec.cy.js
@@ -1,4 +1,4 @@
-describe('Waits ', () => {
+describe('Primer mensaje - Waits', () => {
 
     it('Deberia permitir al usuario verificar el primer mensaje', () => {
         cy.log('Verificar primer mensaje')
