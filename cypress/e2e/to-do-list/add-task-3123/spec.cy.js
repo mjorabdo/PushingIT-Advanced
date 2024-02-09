ec39610853a175a0dc1527a084f687725f926897
Fabrizio Otranto <Fabryotranto@gmail.com>
2024-02-08 21:28:22 -0300

    Revert "commit para hacer revert"
    This reverts commit b22b674be527aae9213a0f8635e7879ba366d703.

--- cypress/e2e/to-do-list/add-task-3123/spec.cy.js
+++ cypress/e2e/to-do-list/add-task-3123/spec.cy.js
@@ -2,7 +2,5 @@ describe(' Agregar tarea - To Do List ', () => {
 
     it('Deberia permitir al usuario agregar una tarea', () => {
         cy.log('Agregar la tarea')
-        cy.log('Agregar la tarea 2')
-
     });
 });
